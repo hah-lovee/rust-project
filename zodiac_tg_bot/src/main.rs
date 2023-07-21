637f34a487a48989e9111a13c499142eacf4024d
hah-love <legyx1@gmail.com>
2023-07-21 13:32:58 +0300

    He came to life

--- zodiac_tg_bot/src/main.rs
+++ zodiac_tg_bot/src/main.rs
@@ -1,20 +1,29 @@
 use teloxide::prelude::*;
 use dotenv::dotenv;
-// dotenv().ok();
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
+    log::info!("Starting throw dice bot...");
+
     let bot = Bot::from_env().auto_send();
 
-    // let bot = Bot::new("").auto_send();
+    // let your_id = ChatId(704343182);
+    // bot.send_message(your_id, "Hi!").await?;    
 
-    let your_id = ChatId(704343182);
-    bot.send_message(your_id, "Hi!").await?;    
-    // `.await` is needed to wait for an async operation
-    // `?` propagates possible errors
+
+    teloxide::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
+        // There are non-text messages, so we need to use pattern matching
+        if let Some(text) = message.text() {
+            // Echo text back into the chat
+            println!("{text}");
+            bot.send_message(message.chat.id, text).await?;
+        }
+    
+        // respond is an alias to `Ok()` with a error type compatible with teloxide
+        respond(())
+    }).await;
 
         Ok(())
 }
