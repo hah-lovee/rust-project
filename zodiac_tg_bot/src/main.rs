82ec33df310b75e52befd43010679723cf3ba2c2
hah-love <legyx1@gmail.com>
2023-07-27 20:47:58 +0300

    compiles, but doesn't work

--- zodiac_tg_bot/src/main.rs
+++ zodiac_tg_bot/src/main.rs
@@ -1,29 +1,148 @@
-use teloxide::prelude::*;
+use teloxide::{Bot, types::Message};
+use tokio::time::sleep;
+
+use lazy_static::lazy_static;
+
+use std::sync::Mutex as std_Mutex;
+use std::time::Duration;
+use std::collections::HashMap;
+use std::sync::Arc;
+
 use dotenv::dotenv;
 
+use teloxide::types::Me;
+use teloxide::utils::command::BotCommands;
+use teloxide::{dispatching::dialogue::InMemStorage, prelude::*};
+use tokio::sync::mpsc::Sender;
+use tokio::sync::Mutex;
+
+type MyDialogue = Dialogue<State, InMemStorage<State>>;
+type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
+
+struct QuestionsHolder(Vec<&'static str>);
+
+// lazy_static! {
+//     // Создаем статическую переменную с типом Mutex<QuestionsHolder>
+//     static QUESTIONS: &[&'static str] = &[
+//     "Question 1: What is 2 + 2?",
+//     "Question 2: What is the capital of France?",
+// ]   ));
+// }
+
+static QUESTIONS: &[&'static str] = &[
+    "Question 1: What is 2 + 2?",
+    "Question 2: What is the capital of France?",
+];
+
+#[derive(Debug)]
+pub enum TaskCommand {
+    Enable,
+    Disable,
+    Delete,
+}
+
+
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "выводит приведственные слова")]
+    Start,
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "Start the test.")]
+    Test,
+
+
+}
+
+#[derive(Clone, Default)]
+pub enum State {
+    #[default]
+    Start,
+    Test,
+}
+
+pub type Store = Arc<Mutex<HashMap<String, Sender<TaskCommand>>>>;
+
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
     dotenv().ok();
 
-    log::info!("Starting throw dice bot...");
 
-    let bot = Bot::from_env().auto_send();
+    let bot = Bot::from_env();
 
-    // let your_id = ChatId(704343182);
-    // bot.send_message(your_id, "Hi!").await?;    
+    let notifys: Store = Arc::new(Mutex::new(HashMap::new()));
 
 
-    teloxide::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
-        // There are non-text messages, so we need to use pattern matching
-        if let Some(text) = message.text() {
-            // Echo text back into the chat
-            println!("{text}");
-            bot.send_message(message.chat.id, text).await?;
+    println!("Starting...");
+
+    let handler = Update::filter_message()
+        .enter_dialogue::<Message, InMemStorage<State>, State>()
+        .branch(dptree::case![State::Start].endpoint(command_handler))
+        .branch(dptree::case![State::Test].endpoint(test_handler))
+        ;
+
+
+    Dispatcher::builder(bot, handler)
+        .dependencies(dptree::deps![InMemStorage::<State>::new(), notifys])
+        // .dependencies(dptree::deps![InMemStorage::<Store>::new()])
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
+
+}
+
+
+
+async fn test_handler(bot: Bot, msg: Message, dialogue: MyDialogue) -> HandlerResult {
+
+
+    for &question in QUESTIONS {
+        let cloned_bot = bot.clone();
+
+        teloxide::repl(cloned_bot, move |message: Message, bot: AutoSend<Bot>| async move {
+            bot.send_message(msg.chat.id, question.to_string()).await;
+            // There are non-text messages, so we need to use pattern matching
+            if let Some(text) = message.text() {
+                // Echo text back into the chat
+                bot.send_message(message.chat.id, "hi").await?;
+            }
+        
+            // respond is an alias to `Ok()` with a error type compatible with teloxide
+            respond(())
+        }).await;
+        
+    }
+    Ok(())
+}
+
+async fn command_handler(bot: Bot, msg: Message, dialogue: MyDialogue, me: Me) -> HandlerResult {
+    if let Some(text) = msg.text() {
+        match BotCommands::parse(text, me.username()) {
+            Ok(Command::Help) => {
+                // Just send the description of all commands.
+                bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?;
+            }
+
+            Ok(Command::Test) => {
+                dialogue.update(State::Test).await?;
+                
+                bot.send_message(msg.chat.id, "test".to_string()).await?;
+
+            }
+
+            Ok(Command::Start) => {
+                bot.send_message(msg.chat.id, "start".to_string()).await?;
+
+            }
+
+            Err(_) => {
+                bot.send_message(msg.chat.id, "Command not found!").await?;
+            }
         }
-    
-        // respond is an alias to `Ok()` with a error type compatible with teloxide
-        respond(())
-    }).await;
+    }
 
-        Ok(())
+    Ok(())
 }
