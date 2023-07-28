06055d084996095997976acfd4b68572c225925b
Denis Bykov <z.bykov.d@gmail.com>
2023-07-28 06:19:29 +0300

    Clean code from unnecessary dependencies

--- zodiac_tg_bot/src/main.rs
+++ zodiac_tg_bot/src/main.rs
@@ -1,34 +1,15 @@
-use teloxide::{Bot, types::Message};
-use tokio::time::sleep;
-
-use lazy_static::lazy_static;
-
-use std::sync::Mutex as std_Mutex;
-use std::time::Duration;
-use std::collections::HashMap;
-use std::sync::Arc;
-
-use dotenv::dotenv;
+use teloxide::{types::Message, Bot};
 
 use teloxide::types::Me;
 use teloxide::utils::command::BotCommands;
 use teloxide::{dispatching::dialogue::InMemStorage, prelude::*};
-use tokio::sync::mpsc::Sender;
-use tokio::sync::Mutex;
 
 type MyDialogue = Dialogue<State, InMemStorage<State>>;
 type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
 
 struct QuestionsHolder(Vec<&'static str>);
 
-// lazy_static! {
-//     static QUESTIONS: &[&'static str] = &[
-//     "Question 1: What is 2 + 2?",
-//     "Question 2: What is the capital of France?",
-// ]   ));
-// }
-
-static QUESTIONS: &[&'static str] = &[
+const QUESTIONS: &[&'static str] = &[
     "Question 1: What is 2 + 2?",
     "Question 2: What is the capital of France?",
 ];
@@ -40,9 +21,11 @@ pub enum TaskCommand {
     Delete,
 }
 
-
 #[derive(BotCommands, Clone)]
-#[command(rename_rule = "lowercase", description = "These commands are supported:")]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
 enum Command {
     #[command(description = "выводит приведственные слова")]
     Start,
@@ -50,8 +33,6 @@ enum Command {
     Help,
     #[command(description = "Start the test.")]
     Test,
-
-
 }
 
 #[derive(Clone, Default)]
@@ -61,55 +42,46 @@ pub enum State {
     Test,
 }
 
-pub type Store = Arc<Mutex<HashMap<String, Sender<TaskCommand>>>>;
-
-
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
+    let token = dotenvy::var("TELOXIDE_TOKEN").unwrap();
 
-
-    let bot = Bot::from_env();
-
-    let notifys: Store = Arc::new(Mutex::new(HashMap::new()));
-
+    let bot = Bot::new(token);
 
     println!("Starting...");
 
     let handler = Update::filter_message()
         .enter_dialogue::<Message, InMemStorage<State>, State>()
         .branch(dptree::case![State::Start].endpoint(command_handler))
-        .branch(dptree::case![State::Test].endpoint(test_handler))
-        ;
-
+        .branch(dptree::case![State::Test].endpoint(test_handler));
 
     Dispatcher::builder(bot, handler)
-        .dependencies(dptree::deps![InMemStorage::<State>::new(), notifys])
+        .dependencies(dptree::deps![InMemStorage::<State>::new()])
         // .dependencies(dptree::deps![InMemStorage::<Store>::new()])
         .enable_ctrlc_handler()
         .build()
         .dispatch()
         .await;
-
 }
 
-
-
 async fn test_handler(bot: Bot, msg: Message, dialogue: MyDialogue) -> HandlerResult {
-
-
+    println!("Entered test_handler");
     for &question in QUESTIONS {
         let cloned_bot = bot.clone();
+        println!("asking question: {}", question);
 
-        teloxide::repl(cloned_bot, move |message: Message, bot: AutoSend<Bot>| async move {
-            bot.send_message(msg.chat.id, question.to_string()).await;
+        teloxide::repl(cloned_bot, move |message: Message, bot: Bot| async move {
+            bot.send_message(msg.chat.id, question.to_string())
+                .await
+                .unwrap();
+            println!("got answer: {}", message.text().unwrap());
             if let Some(text) = message.text() {
                 bot.send_message(message.chat.id, "hi").await?;
             }
-        
+
             respond(())
-        }).await;
-        
+        })
+        .await;
     }
     Ok(())
 }
@@ -118,19 +90,19 @@ async fn command_handler(bot: Bot, msg: Message, dialogue: MyDialogue, me: Me) -
     if let Some(text) = msg.text() {
         match BotCommands::parse(text, me.username()) {
             Ok(Command::Help) => {
-                bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?;
+                bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                    .await?;
             }
 
             Ok(Command::Test) => {
                 dialogue.update(State::Test).await?;
-                
-                bot.send_message(msg.chat.id, "test".to_string()).await?;
 
+                bot.send_message(msg.chat.id, "test".to_string()).await?;
             }
 
             Ok(Command::Start) => {
                 bot.send_message(msg.chat.id, "start".to_string()).await?;
-
+                // dialogue.update(State::Test).await?;
             }
 
             Err(_) => {
