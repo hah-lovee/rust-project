cd29bd4fb7f644bdbd3af605d1b4c569f1505107
hah-love <legyx1@gmail.com>
2023-08-05 13:55:17 +0300

    writes to a file, but it is necessary to remove global variables

--- zodiac_tg_bot/src/main.rs
+++ zodiac_tg_bot/src/main.rs
@@ -1,15 +1,14 @@
 use std::sync::Mutex;
-use std::env;
+use std::fs::OpenOptions;
 
-use sqlx::postgres::PgPool;
-use sqlx::Pool;
-use sqlx::query;
 
 use teloxide::{types::{Message, Me}, Bot};
 use teloxide::utils::command::BotCommands;
 use teloxide::{dispatching::dialogue::InMemStorage, prelude::*};
 
 use dotenv::dotenv;
+use itertools::Itertools;
+use csv::WriterBuilder;
 
 
 
@@ -58,7 +57,7 @@ pub enum State {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
+async fn main() {
     dotenv().ok();
 
     
@@ -83,7 +82,6 @@ async fn main() -> Result<(), sqlx::Error> {
         .dispatch()
         .await;
 
-    Ok(())
 }
 
 
@@ -113,7 +111,19 @@ async fn test_handler(bot: Bot, msg: Message, dialogue: MyDialogue) -> HandlerRe
                                     vector.push(digit);
                                     println!("{:?}", *vector);
 
-                                    insert_values().await?;
+                                    let file = OpenOptions::new()
+                                        .create(true)
+                                        .append(true)
+                                        .open("../test.csv")
+                                        .expect("Failed to open file");
+
+                                    let values_str = vector.iter().map(|value| value.to_string()).join(",");
+
+                                    let mut csv_writer = WriterBuilder::new()
+                                        .quote_style(csv::QuoteStyle::Never)
+                                        .from_writer(file);
+                                    csv_writer.write_record(&[values_str]).expect("Failed to write to CSV");
+
 
                                     vector.clear();
                                     
@@ -170,54 +180,12 @@ async fn command_handler(bot: Bot, msg: Message, dialogue: MyDialogue, me: Me) -
             Ok(Command::Start) => {
                 let message = "start";
                 bot.send_message(msg.chat.id, message.to_string()).await?;
-                // dialogue.update(State::Test).await?;
             }
 
             Err(_) => {
                 bot.send_message(msg.chat.id, "Command not found!").await?;
             }
         }
-    }
-    Ok(())
-}
-
-async fn insert_values() -> Result<(), sqlx::Error> {
-    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
-
-    let pool = PgPool::connect(&db_url).await?;
-
-    unsafe{
-        if let Some(ref global_vector) = GLOBAL_VECTOR {
-            let mut vector = global_vector.lock().unwrap();
-
-            let first_q = vector[0];
-            let second_q = vector[1];
-            let third_q = vector[2];
-            let fourth_q = vector[3];
-            let fifth_q = vector[4];
-            let sixth_q = vector[5];
-            let seventh_q = vector[6];
-            let eighth_q = vector[7];
-            let ninth_q = vector[8];
-            let tenth_q = vector[9];
-
-            let query = "INSERT INTO test (q1, q2, q3, q4, q5, q6, q7, q8, q9, q10)
-                        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)";
-            sqlx::query(query)
-                .bind(first_q)
-                .bind(second_q)
-                .bind(third_q)
-                .bind(fourth_q)
-                .bind(fifth_q)
-                .bind(sixth_q)
-                .bind(seventh_q)
-                .bind(eighth_q)
-                .bind(ninth_q)
-                .bind(tenth_q)
-                .execute(&pool)
-                .await?;
-            
-        }   
     }
     Ok(())
 }
